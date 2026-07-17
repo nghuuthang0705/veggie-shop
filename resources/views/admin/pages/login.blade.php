0090bcc1dd2c38a960a431e4056ed0884ba20c1d
OrcinusOrca <nghuuthang0705@gmail.com>
2026-07-17 15:49:48 +0700

    add admin authentication middleware

--- resources/views/admin/pages/login.blade.php
+++ resources/views/admin/pages/login.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login Admin</title>
+    <title>Đăng nhập Admin</title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('assets/admin//vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
--- routes/admin.php
+++ routes/admin.php
@@ -5,14 +5,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function() {
 
-    Route::get('/dashboard', function() {
-        return view('admin.pages.dashboard');
-    })->name('admin.dashboard');
+    
 
-    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    Route::middleware(['check.auth.admin'])->group(function() {
+        Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+        Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    });
     
     Route::get('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+    Route::middleware(['auth.custom'])->group(function() {
+        Route::get('/dashboard', function() {
+            return view('admin.pages.dashboard');
+        })->name('admin.dashboard');
+    });
+
+    
 }); 
 
 ?>
